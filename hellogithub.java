3797e22b90ed0eb1bee64043dea2328b108d5c77
c0dehard <47797039+c0dehard@users.noreply.github.com>
2019-02-19 23:19:02 +0100

    Add files via upload

---
+++ hellogithub.java
@@ -0,0 +1,13 @@
+package hellogithub;
+
+/**
+ *
+ * @author c0dehard
+ */
+public class HelloGitHub {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
+    
